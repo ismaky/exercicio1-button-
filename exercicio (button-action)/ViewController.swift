0b27679b84a6ebb783e14c2f80b9565070384df0
ismaky <ismael963828@gmail.com>
2023-02-24 01:29:57 -0300

    button-action1

--- exercicio (button-action)/ViewController.swift
+++ exercicio (button-action)/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var text = "AÇÃO DISPARADA"
 
+    var button: UIButton {
+        let button: UIButton = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("APERTE", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 26)
+        button.setTitleColor(.black, for: .normal)
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderWidth = 4
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(self.tappedButton(_:)), for: .touchUpInside)
+        button.backgroundColor = .black
+        return button
+    }
+    
+    @objc func tappedButton(_ sender: UIButton) {
+        print(text)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+        self.view.addSubview(self.button)
+        self.configConstraints()
     }
 
+    private func configConstraints() {
+        NSLayoutConstraint.activate([
+            self.button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            self.button.heightAnchor.constraint(equalToConstant: 20),
+            self.button.widthAnchor.constraint(equalToConstant: -20)
+        ])
+    }
 
 }
 
